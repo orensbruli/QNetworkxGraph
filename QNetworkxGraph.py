7737749b4d1f43a99b9fe995c1d7d8d253a085c1
orensbruli <orensbruli@gmail.com>
2017-08-05 00:13:02 +0200

    Fixed the separator position

--- QNetworkxGraph.py
+++ QNetworkxGraph.py
@@ -815,8 +815,8 @@ class QNetworkxWidget(QGraphicsView):
         action1 = QAction("Panning mode", self)
         action1.triggered.connect(self.set_panning_mode)
         action1.setCheckable(True)
-        self.menu.addSeparator()
         self.menu.addAction(action1)
+        self.menu.addSeparator()
 
         self.menu = QMenu()
         action1 = QAction("Panning mode", self)
